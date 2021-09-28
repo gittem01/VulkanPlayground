03f18c5864a8a7583ae012d39e8fa522d80d4c9a
gittem01 <mchtt43@gmail.com>
2021-09-28 18:54:16 +0300

    bullet3 physics library added

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <vk_engine.h>
+#include "btBulletDynamicsCommon.h"
 
 float getRand01() {
 	return ((double)rand()) / RAND_MAX;
@@ -21,17 +22,57 @@ void createObjects(VulkanEngine& engine){
 	}
 }
 
+btDiscreteDynamicsWorld* createPhysicsWorld() {
+	///collision configuration contains default setup for memory, collision setup
+	btDefaultCollisionConfiguration* collisionConfiguration = new btDefaultCollisionConfiguration();
+	//m_collisionConfiguration->setConvexConvexMultipointIterations();
+
+	///use the default collision dispatcher. For parallel processing you can use a diffent dispatcher (see Extras/BulletMultiThreaded)
+	btCollisionDispatcher* dispatcher = new btCollisionDispatcher(collisionConfiguration);
+
+	btDbvtBroadphase* broadphase = new btDbvtBroadphase();
+
+	///the default constraint solver. For parallel processing you can use a different solver (see Extras/BulletMultiThreaded)
+	btSequentialImpulseConstraintSolver* sol = new btSequentialImpulseConstraintSolver;
+
+	btDiscreteDynamicsWorld* dynamicsWorld = new btDiscreteDynamicsWorld(dispatcher, broadphase, sol, collisionConfiguration);
+
+	dynamicsWorld->setGravity(btVector3(0, -10, 0));
+
+	btBoxShape* bs = new btBoxShape(btVector3(0.5f, 0.5f, 0.5f));
+	btDefaultMotionState* ms = new btDefaultMotionState();
+	btRigidBody* body = new btRigidBody(1.0f, ms, bs);
+	dynamicsWorld->addRigidBody(body);
+
+	
+	btTransform t;
+	t.setIdentity();
+	t.setOrigin(btVector3(0, -5, 0));
+	btDefaultMotionState* ms2 = new btDefaultMotionState(t);
+	btRigidBody* body2 = new btRigidBody(btScalar(0.), ms2, bs);
+	dynamicsWorld->addRigidBody(body2);
+
+	return dynamicsWorld;
+}
+
 int main(int argc, char* argv[]){
 	VulkanEngine engine(1300, 700);
-
+	
 	engine.get_mesh("../../assets/monkey_flat.obj", "monkey");
 	engine.get_image("../../assets/monkey.png", "monkey");
 
 	createObjects(engine);
+	btDiscreteDynamicsWorld* world = createPhysicsWorld();
+	btRigidBody* body = world->getNonStaticRigidBodies().at(0);
 
 	bool done = false;
 	while (!done) {
 		done = engine.looper();
+
+		// 1x1x1x box starts at (0, 0, 0) and collides with the static box size of 1x1x1 which is at the (0, -5, 0)
+		world->stepSimulation(engine.io->DeltaTime);
+		btVector3 pos = body->getCenterOfMassPosition();
+		printf("body position: %f, %f, %f\n", pos.getX(), pos.getY(), pos.getZ());
 	}
 
 	return 0;
